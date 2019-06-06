6455b1c019a3830a5282a5ab63071f50c32ffe37
Suraj Patel <Surajpatel527@gmail.com>
2019-06-06 19:47:23 -0400

    Changelog: Redo'd the entire file most code stayed the same some is changed for better understanding.

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+var tableArray = [
+    {
+      customerName: "John Smith",
+      customerEmail: "john@example.com",
+      customerID: "JohnSmith98",
+      phoneNumber: "000-000-0000"
+    }
+  ];
+  
+  module.exports = tableArray;
---
+++ data/waitinglistData.js
@@ -0,0 +1,11 @@
+var waitingArray = [
+    {
+      customerName: "God",
+      customerEmail: "god@example.com",
+      phoneNumber: "000-000-0000",
+      customerID: "Omnipotent"
+    }
+  ];
+  
+  
+  module.exports = waitingArray;
